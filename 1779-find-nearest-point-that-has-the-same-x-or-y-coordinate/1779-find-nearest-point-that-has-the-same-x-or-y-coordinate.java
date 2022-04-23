f1f6efdfc5d00752d42d76e90f83d2c381d4a7b1
Md. Atequer Rahman Sourov <sourovbd@users.noreply.github.com>
2022-04-23 22:51:49 +0600

    Time: 3 ms (43.18%), Space: 72.9 MB (36.51%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.java
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.java
@@ -1,23 +1,24 @@
 class Solution {
     public int nearestValidPoint(int x, int y, int[][] points) {
-        Map<Integer, Integer> validPointsMap = new HashMap<>();
-        int len = points.length;
-        int noOfValidArray = 0;
+        int index = -1;
+        int maxDistance = Integer.MAX_VALUE;
         int manhattanDistance = 0;
-        int[][] validArray = new int[len][2];
-        
-        for (int i = 0; i < len; i++) {
-            int[] arr = points[i];
-            if (x == arr[0] || y == arr[1]) {
-                validArray[i][0] = arr[0];
-                validArray[i][1] = arr[1];
-                manhattanDistance = Math.abs(x - validArray[i][0]) + Math.abs(y - validArray[i][1]);
-                validPointsMap.put(i, manhattanDistance);
-                noOfValidArray++;
+        for (int i = 0; i < points.length; i++) {
+            if (points[i][0] == x) {
+                manhattanDistance = Math.abs(points[i][1] - y);
+                if (manhattanDistance < maxDistance) {
+                    maxDistance = manhattanDistance;
+                    index = i;
+                }
+            } else if (points[i][1] == y){
+                manhattanDistance = Math.abs(points[i][0] - x);
+                if (manhattanDistance < maxDistance) {
+                    maxDistance = manhattanDistance;
+                    index = i;
+                }
             }
         }
-        if(noOfValidArray==0) return -1;
-        return validPointsMap.entrySet().stream().min(Comparator.comparingInt(Map.Entry::getValue)).get().getKey();
+        return index;
 
     }
 }
