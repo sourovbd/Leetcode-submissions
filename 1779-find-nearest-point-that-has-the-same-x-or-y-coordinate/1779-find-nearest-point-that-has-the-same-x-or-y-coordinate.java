eb3c4fdf43fc171ab799958e557711cb39f55836
Md. Atequer Rahman Sourov <sourovbd@users.noreply.github.com>
2022-04-23 22:54:34 +0600

    Time: 1 ms (100.00%), Space: 49.9 MB (84.67%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.java
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.java
@@ -1,17 +1,17 @@
 class Solution {
     public int nearestValidPoint(int x, int y, int[][] points) {
-        int index = -1;
+         int index = -1;
         int maxDistance = Integer.MAX_VALUE;
-        int manhattanDistance = 0;
+        
         for (int i = 0; i < points.length; i++) {
             if (points[i][0] == x) {
-                manhattanDistance = Math.abs(points[i][1] - y);
+                int manhattanDistance = Math.abs(points[i][1] - y);
                 if (manhattanDistance < maxDistance) {
                     maxDistance = manhattanDistance;
                     index = i;
                 }
             } else if (points[i][1] == y){
-                manhattanDistance = Math.abs(points[i][0] - x);
+                int manhattanDistance = Math.abs(points[i][0] - x);
                 if (manhattanDistance < maxDistance) {
                     maxDistance = manhattanDistance;
                     index = i;
@@ -19,6 +19,5 @@ class Solution {
             }
         }
         return index;
-
     }
 }
