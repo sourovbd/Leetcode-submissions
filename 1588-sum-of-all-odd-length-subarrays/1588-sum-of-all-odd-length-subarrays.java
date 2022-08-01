a7f19b1370f053193037a39e369d72c5334fd510
Md. Atequer Rahman Sourov <sourovbd@users.noreply.github.com>
2022-08-01 06:33:28 +0600

    Time: 109 ms (5.04%), Space: 68.6 MB (5.25%) - LeetHub

---
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int len = arr.length;
+        List<Integer> term = new ArrayList<>();
+        for (int i=1; i<=len; i=i+2) {
+            term.add(i);
+        }
+        int termLen = term.size();
+
+        List<Integer[]> integers = new ArrayList<>();
+        for (int j=0; j<termLen; j++) {
+            for (int i=0; i<=len-term.get(j); i++) {
+                Integer[] subarray = IntStream.range(i, i + term.get(j))
+                        .mapToObj(k -> arr[k])
+                        .toArray(Integer[]::new);
+                integers.add(subarray);
+            }
+        }
+
+        int sum=0;
+        for (int i=0; i<integers.size(); i++) {
+            Integer[] arr2 = integers.get(i);
+            for(int j=0; j<arr2.length; j++) {
+                sum = sum + arr2[j];
+            }
+        }
+        return sum;
+    }
+}
