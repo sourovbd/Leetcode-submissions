fcba93a4806955eff43d6f76339ad436b49005b1
Md. Atequer Rahman Sourov <sourovbd@users.noreply.github.com>
2022-08-02 07:59:35 +0600

    Time: 13 ms (12.59%), Space: 55.2 MB (20.68%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,14 +1,27 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int len = nums.length;
-       for (int j=0; j<len-1; j++) {
-           for(int i=0; i<len-1; i++) {
-               if(nums[i]==0) {
-                   int temp = nums[i];
-                   nums[i] = nums[i+1];
-                   nums[i+1] = temp;
-               }
-           }
-       }
+      int len = nums.length;
+        int nonZeroCount = 0;
+        List<Integer> list = new ArrayList<>();
+        List<Integer> reOrderedList = new ArrayList<>();
+
+        for (int j=0; j<len; j++) {
+            list.add(nums[j]);
+        }
+        
+        int listLen = list.size();
+        for (int j=0; j<listLen; j++) {
+            if (list.get(j) != 0) {
+                reOrderedList.add(list.get(j));
+                nonZeroCount++;
+            }
+        }
+        for (int j=nonZeroCount; j<listLen; j++) {
+            reOrderedList.add(0);
+        }
+        
+        for (int j=0; j<len; j++) {
+            nums[j] = reOrderedList.get(j);
+        }
     }
 }
