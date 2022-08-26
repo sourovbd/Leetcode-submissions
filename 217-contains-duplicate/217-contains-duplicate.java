8ac1cdeea549da37a7d7d89029fa300c7dd32dbc
Md. Atequer Rahman Sourov <sourovbd@users.noreply.github.com>
2022-08-26 11:20:28 +0600

    Time: 46 ms (5.05%), Space: 75.7 MB (5.24%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // int len = nums.length;
-        // Integer[] NUMS = Arrays.stream(nums).boxed().toArray( Integer[]::new );
-        // Set<Integer> integerSet = Arrays.stream(NUMS).collect(Collectors.toSet());
-        // int setLen = integerSet.size();
-        // return (len==setLen) ? false : true;
-        int len = Arrays.stream(nums).distinct().toArray().length;
-        int len2 = nums.length;
-        return (len != len2) ? true : false;
+        int len = nums.length;
+        Integer[] NUMS = Arrays.stream(nums).boxed().toArray( Integer[]::new );
+        Set<Integer> integerSet = Arrays.stream(NUMS).collect(Collectors.toSet());
+        int setLen = integerSet.size();
+        return (len==setLen) ? false : true;
+        // int len = Arrays.stream(nums).distinct().toArray().length;
+        // int len2 = nums.length;
+        // return (len != len2) ? true : false;
         
     }
 }
