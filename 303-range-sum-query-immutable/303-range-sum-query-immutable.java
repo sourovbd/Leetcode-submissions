d1f85dcbfba2a4732aa8a7876cd7eeed6de8215b
Md. Atequer Rahman Sourov <sourovbd@users.noreply.github.com>
2022-09-02 07:27:47 +0600

    Time: 15 ms (55.12%), Space: 48.9 MB (68.78%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.java
@@ -1,17 +1,23 @@
 class NumArray {
 
-    private int[] nums;
-    
+   // In prefix sum array, simply store the sum of all indexes <= current index of nums array.
+	//Example - nums[] = {3,1,2} then prefixSum[] = {3, 3+1, 3+1+2} i.e {3, 4, 6}.
+	// now prefixSum[i] denotes the sum of all numbers till index i of nums array.
+    int[] prefixSum;
     public NumArray(int[] nums) {
-        this.nums = nums;
-    }
-
-    public int sumRange(int left, int right) {
-        int sum = 0;
-        for (int i=left; i<=right; i++) {
-            sum += nums[i];
+        prefixSum = new int [nums.length];
+        
+        prefixSum[0] = nums[0];
+        for(int i=1; i<nums.length; i++) {
+            prefixSum[i] = prefixSum[i-1] + nums[i];
         }
-        return sum;
+    }
+    
+    public int sumRange(int left, int right) {
+	//Simple logic for range sum i.e Sum till right - sum before left
+        if (left == 0)
+            return prefixSum[right];
+        return prefixSum[right] - prefixSum[left-1];  
     }
 }
 
