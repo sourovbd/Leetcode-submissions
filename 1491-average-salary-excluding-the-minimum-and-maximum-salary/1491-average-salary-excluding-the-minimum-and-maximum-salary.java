471e3eeb3e548620f0e0da1427d296816950078a
Md. Atequer Rahman Sourov <sourovbd@users.noreply.github.com>
2022-09-06 06:56:25 +0600

    Time: 2 ms (9.20%), Space: 39.9 MB (92.16%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -1,11 +1,17 @@
 class Solution {
     public double average(int[] salary) {
-        Arrays.sort(salary);
-        int len = salary.length;
-        salary[0] = 0;
-        salary[len-1] = 0;
-        double sum = Arrays.stream(salary).reduce(0, Integer::sum);;
-
-        return sum / (len - 2);
+        int maxSalary = Arrays.stream(salary).max().getAsInt();
+        int minSalary = Arrays.stream(salary).min().getAsInt();
+        double sumOfSalary = Arrays.stream(salary).sum();
+        return ((sumOfSalary - maxSalary - minSalary) / (salary.length - 2));
     }
 }
+
+// Arrays.sort(salary);
+//         int len = salary.length;
+//         salary[0] = 0;
+//         salary[len-1] = 0;
+//         double sum = Arrays.stream(salary).reduce(0, Integer::sum);;
+
+//         return sum / (len - 2);
+//     }
