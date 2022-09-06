6f1ec3126294132b29f64e35aac28d49e90c980e
Md. Atequer Rahman Sourov <sourovbd@users.noreply.github.com>
2022-09-06 06:47:27 +0600

    Time: 2 ms (9.20%), Space: 41.8 MB (38.83%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -1,36 +1,11 @@
 class Solution {
     public double average(int[] salary) {
-        double average = 0;
-        double sum = 0;
-        int length = salary.length;
-        for (int i = 0; i < length; i++) {
-            sum = sum + salary[i];
-        }
-        int maxSalary = getMax(salary);
-        int minSalary = getMin(salary);
-        double sumOfSalary = sum - maxSalary - minSalary;
-        average = (sumOfSalary / (length-2));
-        return average;
-    }
-    
-    public int getMax(int[] salary) {
-        int max = salary[0];
-        for (int i = 1; i < salary.length; i++) {
-            if(salary[i] > max) {
-                max = salary[i];
-            }
-        }
-        return max;
-    }
+        Arrays.sort(salary);
+        int len = salary.length;
+        salary[0] = 0;
+        salary[len-1] = 0;
+        double sum = Arrays.stream(salary).reduce(0, Integer::sum);;
 
-    public int getMin(int[] salary) {
-        int min = salary[0];
-        for (int i = 1; i < salary.length; i++) {
-            if(salary[i] < min) {
-                min = salary[i];
-            }
-        }
-        return min;
+        return sum / (len - 2);
     }
-
 }
