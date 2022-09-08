c72345681c573e154d24b370182b341397b6df50
Md. Atequer Rahman Sourov <sourovbd@users.noreply.github.com>
2022-09-08 06:17:08 +0600

    Time: 2 ms (8.91%), Space: 43.8 MB (62.51%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -8,7 +8,12 @@ class Solution {
                 nums[i] = 1;
             else if (nums[i] == 0)
                 return 0;
+            multiplication = multiplication * nums[i];
         }
-        return Arrays.stream(nums).reduce(1, (a,b) -> a * b);
+        if (multiplication < 0)
+            return -1;
+        if (multiplication > 0)
+            return 1;
+        return 0;
     }
 }
