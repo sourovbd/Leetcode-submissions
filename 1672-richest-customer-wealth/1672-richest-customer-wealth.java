4967f61e7ab256c6155a176362d6e125613784dc
Md. Atequer Rahman Sourov <sourovbd@users.noreply.github.com>
2022-09-17 07:35:12 +0600

    Time: 2 ms (12.85%), Space: 41.8 MB (90.64%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -4,11 +4,7 @@ class Solution {
         int max =0;
         for (int i=0; i<len2d; i++) {
             int[] arr = accounts[i];
-            int len = arr.length;
-            int sum=0;
-            for (int j=0; j<len; j++) {
-                sum = sum + arr[j];
-            }
+            int sum = Arrays.stream(arr).sum();
             if (sum > max) {
                 max = sum;
             }
