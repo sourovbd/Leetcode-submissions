30f735b53212986fecfe441019961fa1b7fe0356
Md. Atequer Rahman Sourov <sourovbd@users.noreply.github.com>
2022-09-18 10:56:27 +0600

    Time: 31 ms (100.00%), Space: 79 MB (60.00%) - LeetHub

--- 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.java
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.java
@@ -3,14 +3,14 @@ class Solution {
         
         Arrays.sort(players);
         Arrays.sort(trainers);
-        int lenOfPlayers = players.length;
-        int lenOfTrainers = trainers.length;
-        int count = 0, player = 0, trainer=0;
-        while (player<lenOfPlayers && trainer<lenOfTrainers) {
+        int totalNoOfPlayers = players.length;
+        int totalNoOfTrainers = trainers.length;
+        int count = 0, player = 0, trainer = 0;
+        while (player < totalNoOfPlayers && trainer < totalNoOfTrainers) {
             if (players[player] <= trainers[trainer]) {
-               player++;
-               trainer++;
-               count++;
+                player++;
+                trainer++;
+                count++;
             } else {
                 trainer++;
             }
