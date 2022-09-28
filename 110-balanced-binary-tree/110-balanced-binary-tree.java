111507b729d6aefdef2a6336df96631f98c79049
Md. Atequer Rahman Sourov <sourovbd@users.noreply.github.com>
2022-09-28 07:01:00 +0600

    Time: 96 ms (5.05%), Space: 42.6 MB (85.61%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -22,18 +22,21 @@ class Solution {
 		return true;
 	}
     // Create a function to return the “height” of a current subtree using recursion...
-	public int Height(TreeNode root) {
+	public static int Height(TreeNode root) {
         // Base case...
-		if (root == null)  return 0;
+        if (root == null)  return 0;
         // Height of left subtree...
-		int leftHeight = Height(root.left);
-        // Height of height subtree...
-		int rightHight = Height(root.right);
+        int leftHeight = Height(root.left);
+        System.out.println("leftHeight: "+leftHeight);
+        // Height of right subtree...
+        int rightHeight = Height(root.right);
+        System.out.println("rightHeight: "+rightHeight);
         // In case of left subtree or right subtree unbalanced, return -1...
-		if (leftHeight == -1 || rightHight == -1)  return -1;
+        if (leftHeight == -1 || rightHeight == -1)  return -1;
         // If their heights differ by more than ‘1’, return -1...
-        if (Math.abs(leftHeight - rightHight) > 1)  return -1;
+        if (Math.abs(leftHeight - rightHeight) > 1)  return -1;
         // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
-		return Math.max(leftHeight, rightHight) + 1;
+        System.out.println("Math.max(leftHeight, rightHight) + 1: "+Math.max(leftHeight, rightHeight) + 1);
+        return Math.max(leftHeight, rightHeight) + 1;
     }
 }
