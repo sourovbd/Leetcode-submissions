122909f763391cf9435c4e473b6ea7b04a6e9299
Md. Atequer Rahman Sourov <sourovbd@users.noreply.github.com>
2022-09-28 07:01:23 +0600

    Time: 1 ms (99.21%), Space: 44.3 MB (57.31%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -27,16 +27,13 @@ class Solution {
         if (root == null)  return 0;
         // Height of left subtree...
         int leftHeight = Height(root.left);
-        System.out.println("leftHeight: "+leftHeight);
         // Height of right subtree...
         int rightHeight = Height(root.right);
-        System.out.println("rightHeight: "+rightHeight);
         // In case of left subtree or right subtree unbalanced, return -1...
         if (leftHeight == -1 || rightHeight == -1)  return -1;
         // If their heights differ by more than ‘1’, return -1...
         if (Math.abs(leftHeight - rightHeight) > 1)  return -1;
         // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
-        System.out.println("Math.max(leftHeight, rightHight) + 1: "+Math.max(leftHeight, rightHeight) + 1);
         return Math.max(leftHeight, rightHeight) + 1;
     }
 }
